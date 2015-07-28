e0b3897116d9e46fee2d2bd83ce19d2e4d55a954
waka <hiromixx21@sano-hiromujitsu-no-MacBook-Pro.local>
2015-07-28 18:28:48 +0900

    解答をコミット、Gemfileのバージョンを2.1.5に変更、rspecファイルのテストケース追加

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
+  BONUS_COUNT = 3
+
   def self.total_count_for(amount)
-    0
+    # 残り本数
+    rest = amount
+    # 総本数
+    answer = amount
+    while (rest >= BONUS_COUNT) do
+    	answer += rest / BONUS_COUNT
+    	rest = (rest / BONUS_COUNT) + (rest % BONUS_COUNT)
+    end
+    return answer
   end
 end
